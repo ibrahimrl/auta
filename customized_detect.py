320ee974e8c21e00ed1c04a0aecd8aa752bba41f
Ulvi <umammadov2021@ada.edu.az>
2024-02-03 03:49:40 +0100

    import/save bugs fixed

--- customized_detect.py
+++ customized_detect.py
@@ -38,12 +38,15 @@ import cv2
 import torch
 
 FILE = Path(__file__).resolve()
+
+print(FILE)
 ROOT = FILE.parents[0]  # YOLOv5 root directory
+ROOT = os.path.join(ROOT, 'yolov5')
+print(ROOT, type(ROOT))
 if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
-sys.path.append('yolov5/')
 
 from ultralytics.utils.plotting import Annotator, colors, save_one_box
 
@@ -100,6 +103,7 @@ def run(
     vid_stride=1,  # video frame-rate stride
 ):
     source = str(source)
+    print("Source ", source)
     save_img = not nosave and not source.endswith(".txt")  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
     is_url = source.lower().startswith(("rtsp://", "rtmp://", "http://", "https://"))
@@ -127,6 +131,8 @@ def run(
     elif screenshot:
         dataset = LoadScreenshots(source, img_size=imgsz, stride=stride, auto=pt)
     else:
+        print("Source 2", source)
+
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=vid_stride)
     vid_path, vid_writer = [None] * bs, [None] * bs
 
@@ -200,7 +206,7 @@ def run(
 
                 x0, y0, x1, y1, conf, cls = det[indices]
 
-                cv2.imwrite(p, im0[int(y0):int(y1),int(x0):int(x1)])
+                cv2.imwrite(str(p), im0[int(y0):int(y1),int(x0):int(x1)])
 
 
                 # Print results
@@ -261,16 +267,16 @@ def run(
             #         vid_writer[i].write(im0)
 
         # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+    #     LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
 
-    # Print results
-    t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
-    LOGGER.info(f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}" % t)
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ""
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
+    # # Print results
+    # t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
+    # LOGGER.info(f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}" % t)
+    # if save_txt or save_img:
+    #     s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ""
+    #     LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+    # if update:
+    #     strip_optimizer(weights[0])  # update model (to fix SourceChangeWarning)
 
 
 def parse_opt():
