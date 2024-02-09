95b43fece0d3ff0133dad064049ecd1e0944e599
Ulvi <umammadov2021@ada.edu.az>
2024-02-10 00:36:12 +0100

    bug solved

--- detection_inference.py
+++ detection_inference.py
@@ -6,7 +6,7 @@ import sys
 from pathlib import Path
 import cv2
 import torch
-
+import numpy as np
 FILE = Path(__file__).resolve()
 
 print(FILE)
@@ -23,6 +23,8 @@ from utils.general import (
     non_max_suppression,
     scale_boxes,
 )
+from utils.augmentations import (letterbox)
+
 from utils.torch_utils import select_device
 
 
@@ -35,25 +37,36 @@ def load_model(weights, device=""):
 def perform_inference(model, img_path):
     # Read the input image
     img = cv2.imread(img_path)
+    o_img = img.copy()
     if img is None:
         print(f"Error: Unable to read image from {img_path}")
         return None
+      
+    
+    # Inference
+    img_size=640
+    stride=32
+    im = letterbox(img, img_size, stride, auto=True)[0]  # padded resize
+    im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    im = np.ascontiguousarray(im)  # contiguous
 
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = torch.from_numpy(img).to(model.device)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = torch.from_numpy(im).to(model.device)
     img = img.half() if model.fp16 else img.float()  # uint8 to fp16/32
     img /= 255  # 0 - 255 to 0.0 - 1.0
+    if len(img.shape) == 3:
+            img = img[None]  # expand for batch dim
 
-    # Inference
+    
     pred = model(img, augment=False)
 
-    # NMS
-    pred = non_max_suppression(pred, conf_thres=0.25, classes=[2,5,6,7], iou_thres=0.45)
+    # NMS  classes=[2,5,6,7]
+    pred = non_max_suppression(pred, conf_thres=0.25,classes=[2,5,6,7], iou_thres=0.45)
 
     # Process predictions
     for det in pred:
         if len(det):
-            det[:, :4] = scale_boxes(img.shape[2:], det[:, :4], img.shape).round()
+            det[:, :4] = scale_boxes(img.shape[2:], det[:, :4], o_img.shape).round()
 
             widths = det[:, 2] - det[:, 0]
             indices = torch.argmax(widths)
@@ -61,12 +74,15 @@ def perform_inference(model, img_path):
             x0, y0, x1, y1, conf, cls = det[indices]
 
             # Save the cropped image
-            crop_img = img[int(y0):int(y1), int(x0):int(x1)]
-
+            crop_img = o_img[int(y0):int(y1), int(x0):int(x1)]
             return crop_img
+
         else:
             return None
 
 # Example usage in another file:
-# model = load_model("yolov5s.pt", device="cuda")
-# perform_inference(model, "path/to/your/image.jpg", save_crop=True, anonymization=False)
+
+
+# model = load_model("yolov5s.pt", device="cpu")
+# a = perform_inference(model, "../JEM207_Project/raw_data/BMW 116_2007/0.png")
+# print(a)
