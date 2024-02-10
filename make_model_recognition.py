39867994df43a45ade74a8244745e51f2e45fb94
Ibrahim Rahimli <ibrahim.rahimli20016@gmail.com>
2024-02-10 02:35:18 +0100

    Updating make_model_recognition.py

--- make_model_recognition.py
+++ make_model_recognition.py
@@ -1,6 +1,5 @@
 
 
-
 # License: BSD
 # Author: Sasank Chilamkurthy
 
@@ -11,6 +10,7 @@ from torch.optim import lr_scheduler
 import torch.backends.cudnn as cudnn
 import numpy as np
 import torchvision
+import torch.nn.functional as F
 from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
 import time
@@ -32,7 +32,7 @@ def load_model(path):
     model_ft.fc = nn.Linear(num_ftrs, 63)
 
     model_ft.load_state_dict(torch.load(path,map_location=torch.device('cpu')))  # Change 'cpu' to 'cuda' if you're using GPU
-
+    return model_ft
 def read_class_names(file_path):
     """
     Reads a text file and returns a list of lines, preserving the order.
@@ -50,8 +50,9 @@ def read_class_names(file_path):
     except FileNotFoundError:
         print(f"Error: File not found - {file_path}")
         return []
-    
-def forward(model, image, class_names):
+
+def forward( model, img, class_names):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     data_transforms = {
     'train': transforms.Compose([
@@ -62,25 +63,32 @@ def forward(model, image, class_names):
         transforms.Normalize([0.3750, 0.3984, 0.4250], [0.2682, 0.2669, 0.2715])
     ]),
     'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(224),
+        # transforms.CenterCrop(224),
         transforms.Grayscale(num_output_channels=3),
         transforms.ToTensor(),
-        transforms.Normalize([0.3750, 0.3984, 0.4250], [0.2682, 0.2669, 0.2715])
-    ]),}
+        transforms.Normalize([0.3750, 0.3984, 0.4250], [0.2682, 0.2669, 0.2715])]),}
 
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Convert to Pillow image
+    # # Convert to Pillow image
     img = Image.fromarray(rgb_image)
 
-    # Display the image
-    images_so_far += 1
+    transformed_img = data_transforms['val'](img)
 
-    # Preprocess the image and get the prediction
-    img = data_transforms['val'](img).unsqueeze(0)  # Assuming transform is defined elsewhere
-    # inputs = img.to(device)  # Assuming device is defined elsewhere
-    outputs = model(img)
-    _, preds = torch.max(outputs, 1)
+    model.eval()
+    transformed_img = transformed_img.to(device)
+    y_pred = model(transformed_img.unsqueeze(0))
+    y_prob = F.softmax(y_pred, dim = -1)
+    top_pred = y_prob.argmax(1, keepdim = True)
+    prob = round(y_prob.max().item(), 2)
+    print( class_names[top_pred], prob)
 
-    return class_names[preds.item()]
+    return class_names[top_pred]
+
+
+# make_model = load_model('Weights/MakeModel_Rec.pt')
+# car_names = read_class_names('Weights/Class_Names.txt')
+# print(car_names)
+# car_make_model = forward(make_model,cv2.imread('data/input_images/00:46:35_0d5253a5-8233-4073-aff7-06dfbd6186b6.jpg'),car_names)
+# print(car_make_model)
